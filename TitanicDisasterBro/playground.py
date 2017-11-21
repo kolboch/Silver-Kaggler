0ee71a0a86b044cebab99a415860177c6fd54d0d
Karol Bochynski <kolboch@gmail.com>
2017-11-21 17:55:47 +0100

    data correlations visualization, charts and more

--- TitanicDisasterBro/playground.py
+++ TitanicDisasterBro/playground.py
@@ -27,21 +27,54 @@ def main():
     # print('{}'.format(train_df.head()))
     # print('{}'.format(train_df.tail()))
     # print('*' * 40)
-    # train_df.info()
+    train_df.info()
     # print('*'*40)
     # test_df.info()
     # print('*' * 40)
     # print('{}'.format(train_df.describe(percentiles=[.61, .62])))
     print('{}'.format(train_df.describe(include=['O'])))
-    print('{}'.format(
-        train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean()
-            .sort_values(by='Survived', ascending=False)
-    ))
-    print('{}'.format(
-        train_df[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean()
-            .sort_values(by='Survived', ascending=False)
+    # print('{}'.format(
+    #     train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean()
+    #         .sort_values(by='Survived', ascending=False)
+    # ))
+    # print('{}'.format(
+    #     train_df[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean()
+    #         .sort_values(by='Survived', ascending=False)
+    # ))
+    # print('{}'.format(
+    #     train_df[["SibSp", "Survived"]].groupby(['SibSp'], as_index=False).mean()
+    #         .sort_values(by='Survived', ascending=False)
+    # ))
 
+    # print('{}'.format(
+    #     train_df[["Parch", "Survived"]].groupby(['Parch'], as_index=False).mean()
+    #         .sort_values(by='Survived', ascending=False)
+    # ))
+    train_df_age = train_df[["Age", "Survived"]]
+    train_df_age['Age'] = train_df_age['Age'].apply(np.round)
+    print('{}'.format(
+        train_df_age[["Age", "Survived"]].groupby(['Age'], as_index=False).mean()
+            .sort_values(by='Age', ascending=True)
     ))
+    # g = sns.FacetGrid(train_df, col='Survived')
+    # g.map(plt.hist, 'Age', bins=40)
+    #
+    # grid = sns.FacetGrid(train_df, col='Pclass', hue='Survived')
+    # # grid = sns.FacetGrid(train_df, col='Survived', row='Pclass', size=2.2, aspect=1.6)
+    # grid.map(plt.hist, 'Age', alpha=.8, bins=20)
+    # grid.add_legend()
+    #
+    # # grid = sns.FacetGrid(train_df, col='Embarked')
+    # grid = sns.FacetGrid(train_df, row='Embarked', size=2.2, aspect=1.6)
+    # grid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette='deep')
+    # grid.add_legend()
+
+    grid = sns.FacetGrid(train_df, col='Embarked', hue='Survived', palette={0: 'k', 1: 'w'})
+    # grid = sns.FacetGrid(train_df, row='Embarked', col='Survived', size=2.2, aspect=1.6)
+    grid.map(sns.barplot, 'Sex', 'Fare', alpha=.5, ci=None)
+    grid.add_legend()
+
+    plt.show()
 
 
 if __name__ == '__main__':
